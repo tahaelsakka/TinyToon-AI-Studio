fd2cce243db62e4ee9fb8cc9b83002f32bc5ab88
tahaelsakka <taha.elsakka77@gmail.com>
2026-07-04 17:10:33 +0300

    Update make_video.py

--- make_video.py
+++ make_video.py
@@ -1,3 +1,8 @@
-from moviepy import ImageClip
+from moviepy import ImageClip, concatenate_videoclips
 
-print("MoviePy loaded")
+images = []
+
+for image in images:
+    clip = ImageClip(image).with_duration(3)
+
+print("Video module ready")
